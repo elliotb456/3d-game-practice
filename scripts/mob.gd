e7fbeef446fb1510d1015e07404102f384e4a0c7
elliotb456 <91119861+elliotb456@users.noreply.github.com>
2026-09-23 11:57:32 +1000

    Scripting the Mobs
    Added a function to spawn the mobs and have them rotate towards the player rather than snapping to them
    
    Added a function called from a signal, when the mobs leaves the screen, its instance is destroyed to free up memory on mobs that are not in the game anymore

---
+++ scripts/mob.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+# Minimum speed of the mob in meters per second 
+@export var min_speed = 10 
+# Maximum speed of the mob in meters per second 
+@export var max_speed = 18
+
+
+func _physics_process(_delta):
+	move_and_slide()
+
+
+# The function will be called from the Main scene 
+func initialize(start_position, player_position): 
+	# We position the move by placing it at the start_position 
+	# and rotate it towards the player_position, so it looks a the player.	
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	# Rotate this mob randomly within range of -45 and +45 degrees,
+	# so that is doesn't move directly towards the player 
+	rotate_y(randf_range(-PI / 4, PI /4))
+
+	# We calculate a random speed integer
+	var random_speed = randi_range(min_speed, max_speed)
+	# We calculare a forwarad velocity that represents that speed 
+	velocity = Vector3.FORWARD * random_speed
+	# We then rotate the velocity vector based on the mob's Y rotation
+	# in order to move in the direction the mob is looking. 
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+# This fucntion is called when the signal is triggered by leaving the screen
+func _on_visible_on_screen_enabler_3d_screen_exited():
+	# Destroys the instance it's called on, in this case the main Mob node
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,41 +9,43 @@ var target_velocity= Vector3.ZERO
 
 
 func _physics_process(delta):
-    # Local variable to store the input direction
-    var direction = Vector3.ZERO
+	# Local variable to store the input direction
+	var direction = Vector3.ZERO
 
-    # Check for the movement inputs defined in the project settings and update the direction accordingly
-    # In 3D spaces, the X,Z axis is the ground plane, Y is the vertical plane 
-    if Input.is_action_pressed("move_right"):
-        direction.x += 1
-    if Input.is_action_pressed("move_left"):
-        direction.x -= 1
-    if Input.is_action_pressed("move_back"):
-        direction.z += 1 
-    if Input.is_action_pressed("move_forward"):
-        direction.z -= 1
-
-
-    # Normalising the Vectors length 
-    # Prevents the player from moving faster by going diagonally
-    if direction != Vector3.ZERO:
-        # We only normalise the vector if the direction has length > 0, meaning the player is moving 
-        direction = direction.normalized()
-        # Setting the basis property will affect he rotation of the specified node 
-        $Pivot.basis = Basis.looking_at(direction)
+	# Check for the movement inputs defined in the project settings 
+	# and update the direction accordingly
+	# In 3D spaces, the X,Z axis is the ground plane, Y is the vertical plane 
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1 
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
 
 
-    # Updating the velocity 
-    # Ground Velocity 
-    target_velocity.x = direction.x * speed 
-    target_velocity.z = direction.z * speed 
+	# Normalising the Vectors length 
+	# Prevents the player from moving faster by going diagonally
+	if direction != Vector3.ZERO:
+		# We only normalise the vector if the direction has length > 0
+		# meaning the player is moving 
+		direction = direction.normalized()
+		# Setting the basis property will affect he rotation of the specified node 
+		$Pivot.basis = Basis.looking_at(direction)
 
-    # Vertical Velocity 
-    if not is_on_floor(): # Checking if the player is in the air 
-        # If true, fall towards the floor (ie. gravity) 
-        target_velocity.y = target_velocity.y - (fall_accelaration * delta)
 
-    
-    # Moving the character 
-    velocity = target_velocity
-    move_and_slide()
+	# Updating the velocity 
+	# Ground Velocity 
+	target_velocity.x = direction.x * speed 
+	target_velocity.z = direction.z * speed 
+
+	# Vertical Velocity 
+	if not is_on_floor(): # Checking if the player is in the air 
+		# If true, fall towards the floor (ie. gravity) 
+		target_velocity.y = target_velocity.y - (fall_accelaration * delta)
+
+	
+	# Moving the character 
+	velocity = target_velocity
+	move_and_slide()
